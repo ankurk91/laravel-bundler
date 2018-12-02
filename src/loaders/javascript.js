c1636f58a9e819c019f5e796bc02bdbd58bb792b
ankurk91 <pro.ankurk1@gmail.com>
2018-12-02 11:08:32 +0530

    Add babel/runtime to reduce the build size

--- src/loaders/javascript.js
+++ src/loaders/javascript.js
@@ -18,6 +18,10 @@ module.exports = {
         }
       ]
     ],
-    plugins: [],
+    plugins: [
+      ["@babel/plugin-transform-runtime", {
+        "useESModules": true
+      }]
+    ],
   }
 };
