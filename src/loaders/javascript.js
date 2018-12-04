aead9f7aa21af6fa4b82170590daf055fdf044f2
ankurk91 <pro.ankurk1@gmail.com>
2018-12-04 11:15:41 +0530

    Honor user babel configs

--- src/loaders/javascript.js
+++ src/loaders/javascript.js
@@ -1,15 +1,14 @@
 const Helpers = require('../helpers');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-  // Handle js and jsx both
-  test: /\.jsx?$/,
-  loader: 'babel-loader',
-  exclude: /node_modules/,
-  options: {
+function getDefaultBabelConfig() {
+  return {
     presets: [
       [
         '@babel/env',
         {
+          debug: false,
           modules: false,
           forceAllTransforms: Helpers.isProduction(),
           targets: {
@@ -19,5 +18,21 @@ module.exports = {
       ]
     ],
     plugins: [],
-  }
+  };
+}
+
+const fileNames = ['.babelrc', '.babelrc.js', 'babel.config.js'];
+
+function userConfigExists() {
+  return fileNames.some((file) => {
+    return fs.existsSync(path.resolve(process.cwd(), file));
+  })
+}
+
+module.exports = {
+  // Handle js and jsx both
+  test: /\.jsx?$/,
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+  options: userConfigExists() ? {} : getDefaultBabelConfig()
 };
