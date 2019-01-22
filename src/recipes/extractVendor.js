c97f6d5506090dd1c5a17b5a6045ad164ae66164
ankurk91 <pro.ankurk1@gmail.com>
2019-01-22 11:12:31 +0530

    Tweak splitChunks plugin

--- src/recipes/extractVendor.js
+++ src/recipes/extractVendor.js
@@ -10,12 +10,16 @@ module.exports = {
       automaticNameDelimiter: '-',
       cacheGroups: {
         default: false,
+        vendors: false,
+
         vendorScripts: {
           test: /node_modules\/(.*)\.js/,
           name: 'vendor',
           chunks: 'all',//initial, async or all
           enforce: true,
+          priority: 20
         },
+
         /*vendorStyles: {
           name: 'vendor',
           test: /(node_modules).*(.*(\.css|\.s?[ac]ss|))$/,
