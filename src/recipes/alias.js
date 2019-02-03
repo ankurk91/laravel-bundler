d00f668f9e051f6569b3be9452a1f93ae9f0ac4c
ankurk91 <pro.ankurk1@gmail.com>
2019-02-03 12:18:48 +0530

    Change @ alias to resources

--- src/recipes/alias.js
+++ src/recipes/alias.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   resolve: {
     alias: {
-      '@': path.resolve(process.cwd(), 'resources/js'),
+      '@': path.resolve(process.cwd(), 'resources'),
       // allows to load files from composer's vendor directory
       '@vendor': path.resolve(process.cwd(), 'vendor'),
     }
