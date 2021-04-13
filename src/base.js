d31e5ba5890eba70abfbb912cec218621a5c8de4
ankurk91 <pro.ankurk1@gmail.com>
2021-04-13 14:53:28 +0530

    Fix cssnano default preset not found issue

--- src/base.js
+++ src/base.js
@@ -55,7 +55,7 @@ module.exports = {
       new CssMinimizerPlugin({
         minimizerOptions: {
           preset: [
-            'cssnano-preset-default',
+            'default',
             {
               discardComments: {removeAll: true}
             }
