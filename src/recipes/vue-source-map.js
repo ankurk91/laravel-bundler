5614af9a4f3ea4755593412380165c2ebc6ed782
ankurk91 <pro.ankurk1@gmail.com>
2022-04-10 11:44:41 +0530

    feat: add recipe to fix vue source maps

---
+++ src/recipes/vue-source-map.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+// https://github.com/vuejs/vue-loader/issues/146
+// https://github.com/vuejs/vue-cli/issues/2978
+// https://forum.vuejs.org/t/56370
+module.exports = {
+  output: {
+    devtoolModuleFilenameTemplate: info => {
+      let resPath = path.normalize(info.resourcePath)
+      let isVue = resPath.match(/\.vue$/)
+      let isGenerated = info.allLoaders
+
+      let generated = `webpack-generated:///${resPath}?${info.hash}`
+      let vueSource = `vue-source:///${resPath}`
+
+      return isVue && isGenerated ? generated : vueSource
+    },
+    devtoolFallbackModuleFilenameTemplate: 'webpack:///[resource-path]?[hash]'
+  }
+}
