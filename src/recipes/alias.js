04601fe1930aadb98465fb665c56971ea4c25f9d
ankurk91 <pro.ankurk1@gmail.com>
2022-07-02 11:42:49 +0530

    remove @vendor alias

--- src/recipes/alias.js
+++ src/recipes/alias.js
@@ -4,8 +4,6 @@ module.exports = {
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), 'resources'),
-      // allows to load files from composer's vendor directory
-      '@vendor': path.resolve(process.cwd(), 'vendor'),
     }
   },
 };
